39b97cd93c728c2a5861343cbb71d4540d164516
Katherine456719 <alexandra.lung@s.unibuc.ro>
2024-04-11 18:40:20 +0300

    Add filter,sort,pagination,add tag table

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const {verifyToken, generateToken} = require('./jwt');
 const {validate} = require("google-auth-library/build/src/options");
 const session = require('express-session');
 const methodOverride = require('method-override');
+const {isAuthenticated} = require("passport/lib/http/request");
 
 // Middleware pentru a parsa JSON si form-uri codificate in URL
 app.use(bodyParser.json());
@@ -51,13 +52,24 @@ function authenticateJWT(req, res, next) {
         return res.status(401).json({message: 'Unauthorized: Missing JWT token'});
     }
     try {
-        const decoded = verifyToken(token); // Verificarea token-ului utilizând funcția verifyToken
-        req.session.userId = decoded.userId; // Salvarea ID-ului utilizatorului din token în obiectul req
+        const decoded = verifyToken(token); // Verificarea token-ul
+        req.session.userId = decoded.userId;
         next();
     } catch (error) {
         return res.status(401).json({message: 'Unauthorized: Invalid JWT token'});
     }
 }
+
+async function isAdmin(req, res, next) {
+
+    if (req.session.role === "ADMIN") {
+        next();
+    } else {
+        return res.status(401).json({message: 'Unauthorized: Not an admin'});
+    }
+
+}
+
 app.use(methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
         let method = req.body._method
@@ -73,6 +85,7 @@ app.get('/signup', (req, res) => {
 // User signup
 app.post('/signup', async (req, res) => {
     const {email, password, name} = req.body;
+
     try {
         const hashedPassword = await hashPassword(password);
         const user = await prismaClient.user.create({
@@ -84,6 +97,7 @@ app.post('/signup', async (req, res) => {
         console.log('Error creating user!');
         res.redirect('/signup');
     }
+
 });
 
 
@@ -110,6 +124,7 @@ app.post('/login', async (req, res) => {
         console.log('Login successful!');
         req.session.userId = user.id;
         req.session.token = generateToken(user.id)
+        req.session.role = user.role;
         res.redirect('/');
     } catch (error) {
         console.log(error + ' Error logging in!');
@@ -130,11 +145,25 @@ app.get('/logout', (req, res) => {
 
 app.get('/', async (req, res) => {
     try {
-        const posts = await prismaClient.post.findMany({
-            include: {author: true},
+        const {page = 1, perPage = 3, sortOption, authorFilter} = req.query;
+        const offset = (page - 1) * perPage;
+        const totalCount = await prismaClient.post.count();
+        const totalPages = Math.ceil(totalCount / perPage);
+        let posts = await prismaClient.post.findMany({
+            include: {author: true, tags: true},
+            orderBy: {
+                createdAt: sortOption === 'createdAtAsc' ? 'asc' : 'desc'
+            },
+            where: {
+                author: authorFilter ? {
+                    name: {contains: authorFilter}
+                } : undefined
+            },
+            skip: offset,
+            take: perPage
         });
         // console.log(req.session.userId)
-        res.render('index.njk', {posts: posts, auth: isLogin(req), postAuth: req.session.userId });
+        res.render('index.njk', {posts: posts, auth: isLogin(req),  currentPage: parseInt(page) ,totalPages, userId: req.session.userId, role: req.session.role});
     } catch (error) {
         console.error(error);
         res.status(500).json({message: 'Error fetching posts!'});
@@ -143,10 +172,10 @@ app.get('/', async (req, res) => {
 // User CRUD operations
 
 // Read all users (implement access control for admins only)
-app.get('/users', async (req, res) => {
+app.get('/users', authenticateJWT, isAdmin, async (req, res) => {
     try {
         const users = await prismaClient.user.findMany();
-        res.json(users);
+        res.render("users.njk", {users: users});
     } catch (error) {
         console.error(error);
         res.status(500).json({message: 'Error fetching users!'});
@@ -154,33 +183,46 @@ app.get('/users', async (req, res) => {
 });
 
 // Read a specific user by ID
-app.get('/users/:id', async (req, res) => {
+app.get('/user/:id', authenticateJWT, async (req, res) => {
     const {id} = req.params;
     try {
         const user = await prismaClient.user.findUnique({where: {id: parseInt(id)}});
         if (!user) {
             return res.status(404).json({message: 'User not found!'});
         }
-        res.json(user);
+        res.render("user.njk", {user: user});
     } catch (error) {
         console.error(error);
         res.status(500).json({message: 'Error fetching user!'});
     }
 });
-
-// Update a user
-app.put('/users/:id', async (req, res) => {
+app.get('/editprofile/:id', authenticateJWT, async (req, res) => {
     const {id} = req.params;
-    const {email, name, role} = req.body;
+    try {
+        const user = await prismaClient.user.findUnique({where: {id: parseInt(id)}});
+        if (!user) {
+            return res.status(404).json({message: 'User not found!'});
+        }
+        res.render("editProfile.njk", {user: user});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Error fetching user!'});
+    }
+});
+// Update a user
+app.put('/user/:id', authenticateJWT, async (req, res) => {
+    const {id} = req.params;
+    const {name, email, bio} = req.body;
     try {
         const user = await prismaClient.user.update({
             where: {id: parseInt(id)},
-            data: {email, name, role},
+            data: {name, email, bio},
         });
         if (!user) {
             return res.status(404).json({message: 'User not found!'});
         }
-        res.json({message: 'User updated successfully!', user});
+        // res.json({message: 'User updated successfully!', user});
+        res.redirect(`/user/${id}`)
     } catch (error) {
         console.error(error);
         res.status(500).json({message: 'Error updating user!'});
@@ -188,14 +230,15 @@ app.put('/users/:id', async (req, res) => {
 });
 
 // Delete a user (implement access control for admins only)
-app.delete('/users/:id', async (req, res) => {
+app.delete('/user/:id', authenticateJWT, async (req, res) => {
     const {id} = req.params;
     try {
         const user = await prismaClient.user.delete({where: {id: parseInt(id)}});
         if (!user) {
             return res.status(404).json({message: 'User not found!'});
         }
-        res.json({message: 'User deleted successfully!'});
+        console.log('User deleted successfully!');
+        res.redirect('/users');
     } catch (error) {
         console.error(error);
         res.status(500).json({message: 'Error deleting user!'});
@@ -211,14 +254,26 @@ app.get('/post', authenticateJWT, async (req, res) => {
 app.post('/post', authenticateJWT, async (req, res) => {
     try {
         // console.log(req.session.userId)
-        const {title, content, type} = req.body;
+        const {title, content, tags, type} = req.body;
+        const tagNames = tags.split(',').map(tag => tag.trim());
+
+        const tagRecords = await Promise.all(tagNames.map(async tagName => {
+            return prismaClient.tag.upsert({
+                where: {name: tagName},
+                update: {},
+                create: {name: tagName}
+            });
+        }));
+
         await prismaClient.post.create({
             data: {
                 title,
                 content,
                 authorId: req.session.userId,
+                tags: {connect: tagRecords.map(tag => ({id: tag.id}))},
                 type
-            }
+            },
+            include: {tags: true}
         });
         res.redirect('/');
     } catch (error) {
@@ -240,7 +295,7 @@ app.get('/post/:id', async (req, res) => {
         if (!post) {
             return res.status(404).json({message: 'Post not found!'});
         }
-        res.render("post.njk",{post:post, method:"put" })
+        res.render("post.njk", {post: post, method: "put"})
     } catch (error) {
         console.error(error);
         res.status(500).json({message: 'Error fetching post!'});
@@ -248,13 +303,28 @@ app.get('/post/:id', async (req, res) => {
 });
 
 // Update a post
-app.put('/post/:id',authenticateJWT, async (req, res) => {
+app.put('/post/:id', authenticateJWT, async (req, res) => {
     const {id} = req.params;
-    const {title, content, type} = req.body;
+    const {title, content, tags, type} = req.body;
     try {
+        const tagNames = tags.split(',').map(tag => tag.trim());
+        console.log(tags)
+        const tagRecords = await Promise.all(tagNames.map(async tagName => {
+            return prismaClient.tag.upsert({
+                where: {name: tagName},
+                update: {},
+                create: {name: tagName}
+            });
+        }));
         const post = await prismaClient.post.update({
             where: {id: parseInt(id)},
-            data: {title, content, type},
+            data: {
+                title,
+                content,
+                tags: {connect: tagRecords.map(tag => ({id: tag.id}))},
+                type
+            },
+            include:{ tags:true}
         });
         if (!post) {
             return res.status(404).json({message: 'Post not found!'});
@@ -269,7 +339,7 @@ app.put('/post/:id',authenticateJWT, async (req, res) => {
 });
 
 // Delete a post
-app.delete('/post/:id',authenticateJWT, async (req, res) => {
+app.delete('/post/:id', authenticateJWT, async (req, res) => {
     const {id} = req.params;
     try {
         const post = await prismaClient.post.delete({where: {id: parseInt(id)}});
@@ -288,7 +358,7 @@ app.delete('/post/:id',authenticateJWT, async (req, res) => {
 app.get('/post/:postId/comment', authenticateJWT, async (req, res) => {
     const {postId} = req.params;
     try {
-        res.render('addcomm.njk', {postId:postId});
+        res.render('addcomm.njk', {postId: postId});
     } catch (error) {
         console.error(error);
         res.status(500).json({message: 'Error fetching post for comment form!'});
@@ -321,14 +391,17 @@ app.get('/post/:postId/comments', async (req, res) => {
         const comments = await prismaClient.comment.findMany({
             where: {postId: parseInt(postId)},
         });
-        res.render('comments.njk',{comments:comments});
+        res.render('comments.njk', {
+            comments: comments,
+            auth: isLogin(req),
+            userId: req.session.userId,
+            role: req.session.role
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({message: 'Error fetching comments!'});
     }
 });
-
-
 
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
